4300c65f24c2facde812e1525706e84598d5fe00
eren iridere <eiridere@gmail.com>
2025-09-04 20:37:47 +0300

    project configure and nativeWind entegre

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const index = () => {
+  return (
+    <View>
+      <Text className="text-red-800 text-3xl">index</Text>
+    </View>
+  );
+};
+
+export default index;
